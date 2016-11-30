0d063d29378e4e6452ba9385a464484abfbf148c
restuagungp <restuagungp@gmail.com>
2016-11-30 11:54:57 +0700

    Add files via upload

---
+++ database/migrations/2016_11_01_132611_create_kendaraans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKendaraansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kendaraans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('jenis');
+            $table->string('nama');
+            $table->integer('harga');
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kendaraans');
+    }
+}
